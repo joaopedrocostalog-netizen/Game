d4cf4eed46d0f5e95a95d4c2953bfbf6bc10c450
joaopedrocostalog-netizen <joaopedro.costalog@gmail.com>
2026-09-11 16:50:48 -0300

    feat: connect military industrial base to strategic console

--- src/components/StrategicWarConsole.tsx
+++ src/components/StrategicWarConsole.tsx
@@ -22,6 +22,7 @@ import { InformationWarfarePanel } from './InformationWarfarePanel';
 import { AdaptiveEnemyCommandPanel } from './AdaptiveEnemyCommandPanel';
 import { MilitaryLegacyPanel } from './MilitaryLegacyPanel';
 import { PeacetimeMilitaryReformsPanel } from './PeacetimeMilitaryReformsPanel';
+import { MilitaryIndustryPanel } from './MilitaryIndustryPanel';
 import { WarConsole } from './WarConsole';
 
 type Props = {
@@ -51,6 +52,7 @@ export function StrategicWarConsole(props: Props) {
     <AdaptiveEnemyCommandPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} territorialControl={props.territorialControl} onArmyStateChange={props.onArmyStateChange} onWarStateChange={props.onWarStateChange}/>
     <MilitaryLegacyPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} territorialControl={props.territorialControl} onArmyStateChange={props.onArmyStateChange}/>
     <PeacetimeMilitaryReformsPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} onArmyStateChange={props.onArmyStateChange}/>
+    <MilitaryIndustryPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} onArmyStateChange={props.onArmyStateChange}/>
     <OperationalCampaignPlansPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} onArmyStateChange={props.onArmyStateChange} onWarStateChange={props.onWarStateChange}/>
     <CasusBelliPlanner entity={entity} entities={props.entities} simulation={props.simulation} onMobilize={props.onMobilize} onDeclareWar={props.onDeclareWar}/>
     <CrisisConferencePanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} warState={props.warState} onWarStateChange={props.onWarStateChange}/>
