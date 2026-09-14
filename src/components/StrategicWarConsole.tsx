9e342c035bf86c4171ac1fcef144b2614fe95b83
joaopedrocostalog-netizen <joaopedro.costalog@gmail.com>
2026-09-14 09:08:31 -0300

    feat: connect naval construction and warfare to strategic console

--- src/components/StrategicWarConsole.tsx
+++ src/components/StrategicWarConsole.tsx
@@ -25,6 +25,7 @@ import { PeacetimeMilitaryReformsPanel } from './PeacetimeMilitaryReformsPanel';
 import { StrategicResourcesPanel } from './StrategicResourcesPanel';
 import { TradePressurePanel } from './TradePressurePanel';
 import { NavalForcesPanel } from './NavalForcesPanel';
+import { NavalWarfarePanel } from './NavalWarfarePanel';
 import { MilitaryIndustryPanel } from './MilitaryIndustryPanel';
 import { WarConsole } from './WarConsole';
 
@@ -58,6 +59,7 @@ export function StrategicWarConsole(props: Props) {
     <StrategicResourcesPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} warState={props.warState}/>
     <TradePressurePanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} warState={props.warState}/>
     <NavalForcesPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} warState={props.warState}/>
+    <NavalWarfarePanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} warState={props.warState}/>
     <MilitaryIndustryPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} onArmyStateChange={props.onArmyStateChange}/>
     <OperationalCampaignPlansPanel entityId={props.entityId} entities={props.entities} simulation={props.simulation} armyState={props.armyState} warState={props.warState} onArmyStateChange={props.onArmyStateChange} onWarStateChange={props.onWarStateChange}/>
     <CasusBelliPlanner entity={entity} entities={props.entities} simulation={props.simulation} onMobilize={props.onMobilize} onDeclareWar={props.onDeclareWar}/>
